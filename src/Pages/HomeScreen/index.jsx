31b606098f6e6a92ca4732444f97348709e2a71e
Marco <marco.boutchelski@gmail.com>
2023-09-11 19:14:32 +0100

    Hero section web w/o completed image

--- src/Pages/HomeScreen/index.jsx
+++ src/Pages/HomeScreen/index.jsx
@@ -9,11 +9,11 @@ export default function Home() {
    return (
       <>
          <HeroSection />
-         <About />
+         {/* <About />
          <Skills />
          <Portfolio />
          <Contact />
-         <Footer />
+         <Footer /> */}
       </>
    )
 }
