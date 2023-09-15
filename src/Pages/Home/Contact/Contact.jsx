c31adefafbf537990a600f6cf05002c217aafa2c
Marco <marco.boutchelski@gmail.com>
2023-09-15 21:16:15 +0100

    Contact me section mobile

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,36 +1,67 @@
 import "./contact.css"
+import { HiOutlineMail, HiOutlineArrowSmRight } from 'react-icons/hi'
 
 export default function Contact() {
    return (
-      <section id="Contact" className="contact--section">
-         <div>
-            <p className="sub--title">Get In Touch</p>
-            <h2>Contact Me</h2>
-            <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, dolor praesentium accusamus deserunt illum ab cumque quisquam saepe expedita odio, aspernatur assumenda magni repellendus impedit quas neque, ut quia rem?</p>
+      <section id="Contact" className="contact--section section">
+         <div className="contact--titles--container">
+            <p className="section--subtitle">Contact Me</p>
+            <h2 className="section--title">Get In Touch</h2>
          </div>
-         <form className="contact--form--container">
-            <div className="container">
-               <label htmlFor="first-name" className="contact--label">
-                  <span className="text-md">First Name</span>
-                  <input type="text" className="contact--input text-md" name="first-name" id="first-name" required />
-               </label>
-               <label htmlFor="last-name" className="contact--label">
-                  <span className="text-md">Last Name</span>
-                  <input type="text" className="contact--input text-md" name="last-name" id="last-name" required />
-               </label>
+         <div className="contact--grid">
+            <div className="email--me--section">
+               {/* <h3 className="contact--title">Email me</h3> */}
+               <div className="contact--content">
+                  <HiOutlineMail className="contact--card--icon" />
+
+                  <h3 className="contact--card-title">Email</h3>
+                  <span className="contact--card--data">marco.boutchelski@gmail.com</span>
+
+                  <a href="mailto:marco.boutchelski@gmail.com" className="contact--button">
+                     Email Me{" "}
+                     <HiOutlineArrowSmRight className="link--button" />
+                  </a>
+               </div>
             </div>
-            <label htmlFor="email" className="contact--label">
-               <span className="text-md">Email</span>
-               <input type="email" className="contact--input text-md" name="email" id="email" required />
-            </label>
-            <label htmlFor="message" className="contact--label">
-               <span className="text-md">Message</span>
-               <textarea className="contact--input text-md" id="message" rows="8" placeholder="Type your message..." />
-            </label>
-            <div>
-               <button className="btn btn-primary contact--form--btn">Submit</button>
-            </div>
-         </form>
+
+            <form className="contact--form--container">
+               <label htmlFor="name" className="contact--label">
+                  <span className="text-md">Name</span>
+                  <input
+                     type="text"
+                     className="contact--input text-md"
+                     name="name"
+                     id="name"
+                     placeholder="Type your name"
+                     required
+                  />
+               </label>
+               <label htmlFor="email" className="contact--label">
+                  <span className="text-md">Email</span>
+                  <input
+                     type="email"
+                     className="contact--input text-md"
+                     name="email"
+                     id="email"
+                     placeholder="Type your email"
+                     required
+                  />
+               </label>
+               <label htmlFor="message" className="contact--label">
+                  <span className="text-md">Message</span>
+                  <textarea
+                     className="contact--input text-md"
+                     id="message"
+                     rows="8"
+                     placeholder="Type your message..."
+                  />
+               </label>
+
+               <div className="submit--button--section">
+                  <button className="btn btn-primary contact--form--btn">Submit</button>
+               </div>
+            </form>
+         </div>
       </section >
    )
 }
--- src/Pages/HomeScreen/index.jsx
+++ src/Pages/HomeScreen/index.jsx
@@ -12,8 +12,8 @@ export default function Home() {
          <About />
          <Skills />
          <Portfolio />
-         {/* <Contact />
-         <Footer /> */}
+         <Contact />
+         {/* <Footer /> */}
       </>
    )
 }
